b84eb6be495befc646e65ad9e203fc957e00bf64
zerokinetic <mhemansakkthivel@gmail.com>
2025-07-04 18:09:07 +0530

    implemented accounts

--- programs/lending-protocol/src/lib.rs
+++ programs/lending-protocol/src/lib.rs
@@ -1,16 +1,14 @@
 use anchor_lang::prelude::*;
 
+mod state;
+
 declare_id!("5XkQCsgqwCfs2NWyj8fABGAsY6EQFxWhQHsJieQASoVj");
 
 #[program]
 pub mod lending_protocol {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
-    }
+    
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
+
