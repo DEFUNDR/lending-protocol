a7b3468a3ce94de84214894ebb4ee18f4fb54e26
zerokinetic <mhemansakkthivel@gmail.com>
2025-07-06 15:11:33 +0530

    implemented deposit_struct

---
+++ programs/lending-protocol/src/instructions/deposit.rs
@@ -0,0 +1,45 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token_interface::{Mint, TokenInterface, InterfaceAccount, TokenAccount};
+use anchor_spl::associated_token::AssociatedToken;
+use create::state::{Bank, User};
+
+#[derive(Accounts)]
+
+pub struct Deposit<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub mint: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        mut,
+        seeds = [mint.key().as_ref()],
+        bump,
+    )]
+    pub bank: Account<'info, Bank>,
+
+    #[account(
+        mut,
+        seeds = [b"treasury", bank.key().as_ref()],
+        bump,
+    )]
+    pub bank_token_account: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        seeds = [signer.key().as_ref()],
+        bump,
+    )]
+    pub user_account: InterfaceAccount<'info, User>
+
+    #[account(
+        mut,
+        associated_token::mint = mint,
+        associated_token::authority = signer,
+        associated_token::token_program = token_program,
+    )]
+    pub user_token_account: InterfaceAccount<'info, TokenAccount>,
+
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
