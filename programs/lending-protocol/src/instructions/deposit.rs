e14de30c2fbe04e5e094a9e00004ebf409d964d5
zerokinetic <mhemansakkthivel@gmail.com>
2025-07-06 15:21:26 +0530

    transfer from user to bank

--- programs/lending-protocol/src/instructions/deposit.rs
+++ programs/lending-protocol/src/instructions/deposit.rs
@@ -1,5 +1,5 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token_interface::{Mint, TokenInterface, InterfaceAccount, TokenAccount};
+use anchor_spl::token_interface::{Mint, TokenInterface, InterfaceAccount, TokenAccount, TransferChecked};
 use anchor_spl::associated_token::AssociatedToken;
 use create::state::{Bank, User};
 
@@ -42,4 +42,23 @@ pub struct Deposit<'info> {
     pub token_program: Interface<'info, TokenInterface>,
     pub system_program: Program<'info, System>,
     pub associated_token_program: Program<'info, AssociatedToken>,
+}
+
+pub fn process_deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
+    let transfer_cpi_accounts = TransferChecked {
+        from: ctx.accounts.user_token_account.to_account_info(),
+        to: ctx.accounts.bank_token_account.to_account_info(),
+        authority: ctx.accounts.signer.to_account_info(),
+        mint: ctx.accounts.mint.to_account_info(),
+    };
+
+    let cpi_program = ctx.accounts.token_program.to_account_info();
+    let cpi_ctx = CpiContext::new(cpi_program, transfer_cpi_accounts);
+
+    let decimals = ctx.accounts.mint.decimals;
+    token_interface::transfer_checked(
+        cpi_ctx,
+        amount,
+        decimals,
+    )?;
 }
