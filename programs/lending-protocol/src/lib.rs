89c181fac5f0df59a9f6081771c10d158464197d
zerokinetic <mhemansakkthivel@gmail.com>
2025-07-06 14:29:51 +0530

    added process_init_user

--- programs/lending-protocol/src/lib.rs
+++ programs/lending-protocol/src/lib.rs
@@ -9,7 +9,13 @@ declare_id!("5XkQCsgqwCfs2NWyj8fABGAsY6EQFxWhQHsJieQASoVj");
 pub mod lending_protocol {
     use super::*;
 
-    
+    pub fn init_bank(ctx: Context<Initialize>, liquidation_threshold: u64, max_ltv: u64) -> Result<()> {
+        process_init_bank(ctx, liquidation_threshold, max_ltv)
+    }
+
+    pub fn init_user(ctx: Context<InitUser>, usdc_address: Pubkey) -> Result<()> {
+        process_init_user(ctx, usdc_address)
+    }
 }
 
 
